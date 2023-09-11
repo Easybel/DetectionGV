e0a8102aa16871f91ce3a99ba7f7d2fd61ecc8a1
ACCakut <7684542+ACCakut@users.noreply.github.com>
2023-09-11 13:09:12 +0200

    Bugfix 1_QCScript.sh
    fixed typo

--- 0_WGSPipeline/1_QCScript.sh
+++ 0_WGSPipeline/1_QCScript.sh
@@ -31,7 +31,7 @@ cd $FastQCFold
 
 # Trim the raw reads with the list of adapters provided in the folder $TrimmFold
 cd $TrimmFold
-java -Xmx30G -Xms24G -jar trimmomatic-0.36.jar PE -threads 8 -trimlog $ID.TrimLog $myDataRaw/$ID"_1.fastq.gz" $myDataRaw/$ID"_2.fastq.gz" $myDataTrim/$ID"_1P".fastq.gz $myDataTrim $ID"_1U".fastq.gz $myDataTrim/$ID"_2P".fastq.gz $myDataTrim/$ID"_2U".fastq.gz ILLUMINACLIP:$TrimmFold/adapters/TruSeq3-PE-2.fa:2:30:10:4 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+java -Xmx30G -Xms24G -jar trimmomatic-0.36.jar PE -threads 8 -trimlog $ID.TrimLog $myDataRaw/$ID"_1.fastq.gz" $myDataRaw/$ID"_2.fastq.gz" $myDataTrim/$ID"_1P".fastq.gz $myDataTrim/$ID"_1U".fastq.gz $myDataTrim/$ID"_2P".fastq.gz $myDataTrim/$ID"_2U".fastq.gz ILLUMINACLIP:$TrimmFold/adapters/TruSeq3-PE-2.fa:2:30:10:4 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
 # Perform FastQC quality control again on trimmed data.
 cd $FastQCFold
